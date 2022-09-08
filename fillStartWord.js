e9d5105c1f93a070d59f0d3590dfe56512d05c4a
BesttJTL <111588284+BesttJTL@users.noreply.github.com>
2022-09-09 00:46:21 +0700

    finish

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,25 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+    if(word==null || word==undefined){
+        console.log (undefined)
+        return undefined
+    }
+    /* ยัง งง อยู่ครับผมว่าทำไมต้องใช้ includes เพื่อนสอนยังไม่ค่อยเข้าใจ
+    วอนผู้รู้ช่วย comment ทีครับผม */
+    else if(word.includes(startWord)){
+    console.log(word)
+    return word
+   }
+   else{
+    console.log(word=startWord+word)
+    return word=startWord+word
+   }
 }
 module.exports = fillStartWord
+
+fillStartWord("1-2565-","helloWorld")
+fillStartWord("JS","beginner")
+fillStartWord("first","firststep")
+fillStartWord("first",null)
+fillStartWord("first",undefined)
